e58eedef72ae8493cdc36053533ca8177e32aac4
Cody <98256022+kewdi@users.noreply.github.com>
2022-02-06 00:46:06 -0500

    init
    example

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace item_deletion_example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()=> InitializeComponent();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 child = new Form2();
+
+            child.OnItemDeleted += OnItemDeleted;
+
+            //or could also do lamda expression:
+            //child.OnItemDeleted += (obj, args) =>
+            //{
+            //    
+            //};
+        }
+
+        private void OnItemDeleted(object sender, Form2.ItemDeletedEventArgs args)
+        {
+            string deleted_item_name = args.name;
+            int deleted_item_num = args.number;
+
+            //...
+
+        }
+
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace item_deletion_example
+{
+    public partial class Form2 : Form
+    {
+        public Form2() => InitializeComponent();
+
+        public class ItemDeletedEventArgs : EventArgs
+        {
+            //information to pass back about the item that gets deleted
+            public string name;
+            public int number;
+            public ItemDeletedEventArgs(string a_name, int a_number)
+            {
+                name = a_name;
+                number = a_number;
+            }
+        }
+        public delegate void ItemDeletedEvent(object sender, ItemDeletedEventArgs args);
+        public ItemDeletedEvent OnItemDeleted;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var item = new { name = "ditto", num = 8 };
+
+            ItemDeletedEventArgs item_info = 
+                new ItemDeletedEventArgs(item.name, item.num);
+
+            OnItemDeleted.Invoke(sender, item_info);;
+        }
+    }
+}
